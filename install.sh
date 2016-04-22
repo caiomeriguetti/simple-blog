af54f320314fa17707f4e13f1623fccfa1d15c67
Caio Meriguetti <caiomeriguetti@gmail.com>
2016-04-21 23:39:48 -0300

    improvement to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#node
+wget https://nodejs.org/dist/v4.3.2/node-v4.3.2-linux-x64.tar.xz -O tmp/node-v4.3.2-linux-x64.tar.xz
+sudo mkdir -p /opt/node
+tar -xf tmp/node-v4.3.2-linux-x64.tar.xz -C /opt/node
+update-alternatives --install /usr/bin/node node /opt/node/node-v4.3.2-linux-x64/bin/node 2110
+update-alternatives --install /usr/bin/npm npm /opt/node/node-v4.3.2-linux-x64/bin/npm 2110
+
 #migrations
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
